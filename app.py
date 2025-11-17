37f35515b8546b8340340981972727f3bb182a42
kolupso <ida.94.christensen@gmail.com>
2025-11-17 21:34:57 +0100

    a

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Personlige stemmer KRV 2025", layout="wide")
 
-st.title("Velkommen til valg-dashboardet")
+st.title("Velkommen til opgørelse af personlige stemmer for KRV 2025")
 st.write("Brug menuen i venstre side til at vælge visning:")
 st.markdown("""
 - **Kommuner / Regioner**
