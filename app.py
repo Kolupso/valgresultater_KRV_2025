12b66ad9499c8ed9de1f8d8c108f5a35d856541f
kolupso <ida.94.christensen@gmail.com>
2025-11-17 21:35:17 +0100

    a

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Personlige stemmer KRV 2025", layout="wide")
 st.title("Velkommen til opgørelse af personlige stemmer for KRV 2025")
 st.write("Brug menuen i venstre side til at vælge visning:")
 st.markdown("""
+- **Annas liste**
 - **Kommuner / Regioner**
 - **SFU kandidater**
-- **Annas liste**
 """)
