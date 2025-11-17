1b96ec1827cc1214ed435f548a77ec73708c4246
kolupso <ida.94.christensen@gmail.com>
2025-11-17 21:34:20 +0100

    a

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-# st.set_page_config(page_title="Databricks Dashboard", layout="wide")
-st.set_page_config(page_title="Forside", page_icon="🏙️")
+st.set_page_config(page_title="Personlige stemmer KRV 2025", layout="wide")
 
 st.title("Velkommen til valg-dashboardet")
 st.write("Brug menuen i venstre side til at vælge visning:")
