e61c630e8361e427a63eb27bfa3e30c4796735eb
Kolupso <158560917+Kolupso@users.noreply.github.com>
2025-11-19 12:12:55 +0100

    file from github

--- pages/Annas_overblik.py
+++ pages/Annas_overblik.py
@@ -13,19 +13,22 @@ ACCESS_TOKEN = st.secrets["ACCESS_TOKEN"]
 st.set_page_config(page_title="Annas overblik", layout="wide")
 st.title("Personlige stemmer 2025 - Annas overblik")
 
-query = """
-SELECT  parti as Parti,
-        kandidat as Kandidat,
-        antal_stemmer as `Antal stemmer`
-FROM workspace.valgresultat.personlige_stemmer_krv_2025
-WHERE anna_liste = 'Ja'
-ORDER BY parti, kandidat;
-"""
+# query = """
+# SELECT  parti as Parti,
+#         kandidat as Kandidat,
+#         antal_stemmer as `Antal stemmer`
+# FROM workspace.valgresultat.personlige_stemmer_krv_2025
+# WHERE anna_liste = 'Ja'
+# ORDER BY parti, kandidat;
+# """
 
-with sql.connect(server_hostname=SERVER_HOST, http_path=HTTP_PATH, access_token=ACCESS_TOKEN) as c:
-    with c.cursor() as cur:
-        cur.execute(query)
-        df = pd.DataFrame(cur.fetchall(), columns=[d[0] for d in cur.description])
+# with sql.connect(server_hostname=SERVER_HOST, http_path=HTTP_PATH, access_token=ACCESS_TOKEN) as c:
+#     with c.cursor() as cur:
+#         cur.execute(query)
+#         df = pd.DataFrame(cur.fetchall(), columns=[d[0] for d in cur.description])
+
+url = "https://raw.githubusercontent.com/Kolupso/valgresultater_KRV_2025/refs/heads/main/combined_results.csv"
+df = pd.read_csv(url)
 
 st_autorefresh(interval=30_000)
 st.dataframe(df)
